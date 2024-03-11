dfc56a13c37bca951ee6d6417daad89d4c8af371
Rodrigo Rivas <rodroy019@hotmail.com>
2024-03-10 21:58:59 -0300

    Finish: se realizan las ultimas modificaciones

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -3,16 +3,14 @@ import Formulario from './Formulario'
 
 const Registro = () => {
   return (
-    <>
-      <div class='container'>
-        <h1>Crear tu Cuenta</h1>
-        <div class='social-icon'>
-          <SocialButton />
-        </div>
-        <span>O puedes usar tu Email para Registrarte</span>
-        <Formulario />
+    <div className='container'>
+      <h1>Crear tu Cuenta</h1>
+      <div className='social-icon'>
+        <SocialButton />
       </div>
-    </>
+      <span>O puedes usar tu Email para Registrarte</span>
+      <Formulario />
+    </div>
   )
 }
 
